0a76cb48f9dfd703cf7cc81df596b2a86f7cf142
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2023-08-31 13:14:10 +0200

    Initial commit after cookiecutter

---
+++ sedos_dashboard/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from sedos_dashboard.users.models import User
+from sedos_dashboard.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
