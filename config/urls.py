784e693e8cd5db7cef18802a03654a721e850373
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2023-12-07 10:41:56 +0100

    Implement index view

--- config/urls.py
+++ config/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="index.html"), name="index"),
     path("", include("django_comparison_dashboard.urls")),
     path("", include("django_energysystem_viewer.urls")),
     path(settings.ADMIN_URL, admin.site.urls),
