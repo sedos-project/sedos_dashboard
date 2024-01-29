6f3252f9ab15e87a37dd87d74d878c3ce7dc2d24
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2024-01-29 15:37:41 +0100

    Overwrite logging instructions (testing)

--- config/settings/production.py
+++ config/settings/production.py
@@ -146,6 +146,20 @@ LOGGING = {
     },
 }
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+}
+
 # Sentry
 # ------------------------------------------------------------------------------
 SENTRY_DSN = env("SENTRY_DSN")
