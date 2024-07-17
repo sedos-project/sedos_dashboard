a8e59d134d5e1ef687aebf7ed0aed16f03b3a6af
Hendrik Huyskens <hendrik.huyskens@rl-institut.de>
2024-07-17 09:25:44 +0200

    Remove clickjacking.XFrameOptionsMiddleware in order to allow iFrame integration

--- config/settings/base.py
+++ config/settings/base.py
@@ -140,7 +140,6 @@ MIDDLEWARE = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django_htmx.middleware.HtmxMiddleware",
 ]
 
